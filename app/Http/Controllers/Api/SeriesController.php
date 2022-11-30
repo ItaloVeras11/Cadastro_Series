122968f670327f12a4bc5570857ec784836de233
ItaloVeras11 <italocabralveras@gmail.com>
2022-11-29 22:56:44 -0300

    Create Controller API

--- app/Http/Controllers/Api/SeriesController.php
+++ app/Http/Controllers/Api/SeriesController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Series;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    //
+    public function index()
+    {
+        return Series::all();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\SeriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/series', function () {
-    return [
-
-    ];
-});
+Route::get('/series', [SeriesController::class, 'index' ] );
